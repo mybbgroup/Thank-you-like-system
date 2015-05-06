1cd09c7543e0da6179c0c4ecee8e4ba1fdb07922
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2015-05-06 18:15:49 +0200

    Small corrections in English lang file
    Small corrections in English lang file

--- inc/languages/english/admin/config_thankyoulike.lang.php
+++ inc/languages/english/admin/config_thankyoulike.lang.php
@@ -2,15 +2,14 @@
 /**
  * Thank You / Like System Config Language Pack English 
  * 
- * $Id: config_thankyoulike.lang.php 13 2015-02-16 11:10:10Z - SvePu - $
  */
 
 $l['tyl_info_title'] = "Thank You/Like System";
 $l['tyl_info_desc'] = "Adds option for users to Thank the user for the post or Like the post.";
-$l['tyl_info_desc_url'] ="<br />*Edited for MyBB 1.8 by: {1}<br />*Maintained by: {2} and {3}<br />*Sources: {4}";
+$l['tyl_info_desc_url'] ="<br />*Edited for MyBB 1.8 by: {1}<br />*Maintained by: {2}, {3} and {4}<br />*Sources: {5}";
 $l['tyl_info_desc_recount'] = "Recount Thank Yous/Likes";
 $l['tyl_info_desc_configsettings'] = "Configure Settings";
-$l['tyl_info_desc_warning'] = "Warning! tyl_unlock file located! Uninstalling will remove ALL the thankyou/like data from the database. If you would like to preserve the thankyou/like data while uninstalling then remove the tyl_unlock file from the forum root folder.";
+$l['tyl_info_desc_warning'] = "<strong>Warning! tyl_unlock file located! Uninstalling will remove ALL the thankyou/like data from the database. If you would like to preserve the thankyou/like data while uninstalling then remove the tyl_unlock file from the forum root folder.</strong>";
 
 $l['tyl_title'] = "Thank You/Like System";
 $l['tyl_desc'] = "Settings to customize the Thank You/Like System Plugin";
@@ -22,10 +21,10 @@ $l['tyl_thankslike_title'] = "Thank You or Like";
 $l['tyl_thankslike_desc'] = "Choose if you want to use the Thank You system or the Like System.";
 
 $l['tyl_firstall_title'] = "First Post only or All";
-$l['tyl_firstall_desc'] = "Do you want the thanks or Like to be given on the first post of a thread only or on all the posts of a thread?";
+$l['tyl_firstall_desc'] = "Do you want the Thanks or Like to be given on the first post of a thread only or on all the posts of a thread?";
 
-$l['tyl_firstalloverwrite_title'] = "Special option for Display TYL-Buttons in ALL Posts";
-$l['tyl_firstalloverwrite_desc'] = "Overwrite the above selected option All in certain forums (choose only forums - no categories)";
+$l['tyl_firstalloverwrite_title'] = "Special option for Display ThankYou/Like buttons in ALL Posts";
+$l['tyl_firstalloverwrite_desc'] = "Overwrite the above selected option All in certain forums (choose only forums - no categories!)";
 
 $l['tyl_removing_title'] = "Allow Removing";
 $l['tyl_removing_desc'] = "Do you want to allow the removing of Thanks/Like from a post already Thanked/Liked?";
@@ -34,10 +33,10 @@ $l['tyl_closedthreads_title'] = "Allow in Closed Threads";
 $l['tyl_closedthreads_desc'] = "Do you want to allow to give Thanks/Like in closed threads?";
 
 $l['tyl_exclude_title'] = "Excluded Forums";
-$l['tyl_exclude_desc'] = "Select forums where you do not want the threads and posts to use the thank you/like system. (only forums - no categories)";
+$l['tyl_exclude_desc'] = "Select forums where you do not want the threads and posts to use the Thank you/Like system. (only forums - no categories!)";
 
 $l['tyl_unameformat_title'] = "Format Usernames";
-$l['tyl_unameformat_desc'] = "Do you want to format the usernames in the thank/list according to their groups?";
+$l['tyl_unameformat_desc'] = "Do you want to format the usernames in the Thanks/Like list according to their groups?";
 
 $l['tyl_hideforgroups_title'] = "Hide ThankYou/Like Button";
 $l['tyl_hideforgroups_desc'] = "Select User Groups which cannot see Thanks/Like button.";
@@ -49,11 +48,11 @@ $l['tyl_dtformat_title'] = "Date/Time Format";
 $l['tyl_dtformat_desc'] = "Set the format you want to use to show the Date/Time in the thank/like list.<br />Format is same as the one used by PHP\'s date() function.<br />Example format: m-d-Y h:i A &lt;&lt;will show&gt;&gt; 12-31-2009 12:01 PM";
 
 $l['tyl_sortorder_title'] = "Sort Order";
-$l['tyl_sortorder_desc'] = "Select the sort order for the thanks/like list.";
+$l['tyl_sortorder_desc'] = "Select the sort order for the Thanks/Like list.";
 
 $l['tyl_collapsible_title'] = "Thanks/Like List Collapsible";
 $l['tyl_collapsible_desc'] = "Do you want the thanks/like list to be collapsible (Show/Hide ability)?";
 
 $l['tyl_colldefault_title'] = "Default Collapsible State";
-$l['tyl_colldefault_desc'] = "If you want the list to be collapsible, what is the default state you want it in when the page loads, open or closed(hidden)?";
+$l['tyl_colldefault_desc'] = "If you want the list to be collapsible, what is the default state you want it in when the page loads, open or closed (hidden)?";
 ?>
