aac4d3921febb81d1207407afbce8bb657fdfa2d
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2015-05-06 18:34:05 +0200

    Small correction in ENG lang file #3
    Admin - recount

--- inc/languages/english/admin/tools_thankyoulike_recount.lang.php
+++ inc/languages/english/admin/tools_thankyoulike_recount.lang.php
@@ -2,7 +2,6 @@
 /**
  * Thank You / Like System Recount English Language Pack
  * 
- * $Id: tools_thankyoulike_recount.lang.php 13 2010-09-16 11:10:10Z - G33K - $
  */
 
 $l['tyl_recount'] = "Thank You/Like Recount";
@@ -10,5 +9,5 @@ $l['tyl_recount_desc'] = "Here you can recount the Thank You/Like System data to
 $l['tyl_name'] = "Name";
 $l['tyl_data_per_page'] = "Data Entries Per Page";
 $l['tyl_recount_do_desc'] = "When this is run, the Thank You/Like count for each user and post will be updated to reflect its current live value based on the data in the database.";
-$l['tyl_success_thankyoulike_rebuilt'] = "The Thank Yous/Likes have been recounted successfully.";
+$l['tyl_success_thankyoulike_rebuilt'] = "The Thank You/Likes have been recounted successfully.";
 $l['tyl_confirm_proceed_rebuild'] = "Click \"Proceed\" to continue the Thank You/Like recount process.";
