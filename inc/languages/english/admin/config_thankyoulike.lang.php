ee8026060cf29563a6d83b9eed34127ba00052f7
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2015-05-06 18:30:06 +0200

    Small corrections in ENG lang file #2

--- inc/languages/english/admin/config_thankyoulike.lang.php
+++ inc/languages/english/admin/config_thankyoulike.lang.php
@@ -9,7 +9,7 @@ $l['tyl_info_desc'] = "Adds option for users to Thank the user for the post or L
 $l['tyl_info_desc_url'] ="<br />*Edited for MyBB 1.8 by: {1}<br />*Maintained by: {2}, {3} and {4}<br />*Sources: {5}";
 $l['tyl_info_desc_recount'] = "Recount Thank Yous/Likes";
 $l['tyl_info_desc_configsettings'] = "Configure Settings";
-$l['tyl_info_desc_warning'] = "<strong>Warning! tyl_unlock file located! Uninstalling will remove ALL the thankyou/like data from the database. If you would like to preserve the thankyou/like data while uninstalling then remove the tyl_unlock file from the forum root folder.</strong>";
+$l['tyl_info_desc_warning'] = "<strong>Warning! tyl_unlock file located! Uninstalling will remove ALL the Thank you/Like data from the database. If you would like to preserve the Thank you/Like data while uninstalling then remove the tyl_unlock file from the forum root folder.</ strong>";
 
 $l['tyl_title'] = "Thank You/Like System";
 $l['tyl_desc'] = "Settings to customize the Thank You/Like System Plugin";
@@ -23,7 +23,7 @@ $l['tyl_thankslike_desc'] = "Choose if you want to use the Thank You system or t
 $l['tyl_firstall_title'] = "First Post only or All";
 $l['tyl_firstall_desc'] = "Do you want the Thanks or Like to be given on the first post of a thread only or on all the posts of a thread?";
 
-$l['tyl_firstalloverwrite_title'] = "Special option for Display ThankYou/Like buttons in ALL Posts";
+$l['tyl_firstalloverwrite_title'] = "Special option for Display Thank You/Like buttons in ALL Posts";
 $l['tyl_firstalloverwrite_desc'] = "Overwrite the above selected option All in certain forums (choose only forums - no categories!)";
 
 $l['tyl_removing_title'] = "Allow Removing";
@@ -42,7 +42,7 @@ $l['tyl_hideforgroups_title'] = "Hide ThankYou/Like Button";
 $l['tyl_hideforgroups_desc'] = "Select User Groups which cannot see Thanks/Like button.";
 
 $l['tyl_showdt_title'] = "Show Date/Time";
-$l['tyl_showdt_desc'] = "Do you want to show the Date/Time the thanks/like was received in the Thanks/Like list?";
+$l['tyl_showdt_desc'] = "Do you want to show the Date/Time the Thanks/Like was received in the Thanks/Like list?";
 
 $l['tyl_dtformat_title'] = "Date/Time Format";
 $l['tyl_dtformat_desc'] = "Set the format you want to use to show the Date/Time in the thank/like list.<br />Format is same as the one used by PHP\'s date() function.<br />Example format: m-d-Y h:i A &lt;&lt;will show&gt;&gt; 12-31-2009 12:01 PM";
