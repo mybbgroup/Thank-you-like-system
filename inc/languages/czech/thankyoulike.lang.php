f94a4e11683119a326eacc5fff8ddb7bc3be0771
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2015-05-21 11:35:27 +0200

    Czech language
    Frontend translation

---
+++ inc/languages/czech/thankyoulike.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Thank You / Like System Czech Language Pack
+ * 
+ */
+
+$l['tyl_thankyou'] = "Poděkovat";
+$l['tyl_thanked'] = "Poděkováno";
+$l['tyl_thanks'] = "Poděkování";
+$l['tyl_thanks_given'] = "Poděkování udělil";
+$l['tyl_thanks_rcvd'] = "Poděkování obdržel";
+$l['tyl_thanks_rcvd_bit'] = "{1} v {2} příspěvcích";
+$l['tyl_like'] = "Líbí";
+$l['tyl_likes'] = "Líbí";
+$l['tyl_likes_given'] = "Líbí uděleno";
+$l['tyl_likes_rcvd'] = "Líbí obdrženo";
+$l['tyl_likes_rcvd_bit'] = "{1} v {2} příspěvcích";
+$l['tyl_liked'] = "Líbí";
+
+$l['tyl_users'] = "uživatelé";
+$l['tyl_user'] = "uživatel";
+$l['tyl_says'] = "says";
+$l['tyl_say'] = "say";
+
+$l['tyl_title_ty'] = "<strong>Následující {1} {2} {3} poděkovali {4} za tento příspěvek:</strong>";
+$l['tyl_title_l'] = "<strong>Následující {1} {2} {3} {4} příspěvek:</strong>";
+$l['tyl_title_collapsed_ty'] = "<strong>{1} {2} {3} Thank You to {4} for this post</strong>";
+$l['tyl_title_collapsed_l'] = "<strong>{1} {2} {3} {4}'s post</strong>";
+
+$l['add_ty'] = "Přidat poděkování";
+$l['del_ty'] = "Odebrat poděkování";
+$l['add_l'] = "Líbí";
+$l['del_l'] = "Nelíbí";
+
+$l['tyl_total_thanks_given'] = "Total Thanks Given";
+$l['tyl_total_likes_given'] = "Total Likes Given";
+$l['tyl_total_thanks_rcvd'] = "Total Thanks Received";
+$l['tyl_total_likes_rcvd'] = "Total Likes Received";
+$l['tyl_tylpd_percent_total'] = "{1} {2} per day | {3} percent of total {2}";
+$l['tyl_find_ty_threads'] = "Find All Thanked Threads";
+$l['tyl_find_ty_posts'] = "Find All Thanked Posts";
+$l['tyl_find_l_threads'] = "Find All Liked Threads";
+$l['tyl_find_l_posts'] = "Find All Liked Posts";
+$l['tyl_find_ty_threads_for'] = "Find All Threads Thanked For";
+$l['tyl_find_ty_posts_for'] = "Find All Posts Thanked For";
+$l['tyl_find_l_threads_for'] = "Find All Threads Liked For";
+$l['tyl_find_l_posts_for'] = "Find All Posts Liked For";
+
+$l['tyl_wol_searching'] = "<a href=\"{1}\">Hledání {2}</a>";
+
+$l['tyl_error_invalid_action'] = "You are trying to perform an invalid action.";
+$l['tyl_error_disabled'] = "{1} Feature has been disabled.";
+$l['tyl_error_not_allowed'] = "Not allowed for this post.";
+$l['tyl_error_excluded'] = "Not allowed for this post because this forum has been excluded.";
+$l['tyl_error_own_post'] = "You can not {1} your own post.";
+$l['tyl_error_already_tyled'] = "You have already {1} this post.";
+$l['tyl_error_unknown'] = "There was an unknown error and your {1} was not added.";
+$l['tyl_error_removal_disabled'] = "You can not remove your {1} because removal has been disabled.";
+$l['tyl_error_own_delete'] = "This {1} can not be removed because you are not the user who posted this {1}. You can only delete {1} that YOU added to posts.";
+$l['tyl_error_not_found'] = "The {1} can not be removed because it can not be found. Its possible that it has already been removed.";
+$l['tyl_error_threadclosed'] = "You cannot {1} this post because it has been closed.";
+
+$l['tyl_redirect_tyled'] = "Your {1} has been added to the post";
+$l['tyl_redirect_deleted'] = "Your {1} has been removed from the post";
+$l['tyl_redirect_back'] = "<br />You will now be taken back to the post.";
+
+
+$l['tyl_alert'] = '{1} thanked/liked your post "{2}" ({3})';
+$l['tyl_alert_like'] = '{1} liked your post "{2}" ({3})';
+$l['tyl_alert_thanks'] = '{1} thanked your post "{2}" ({3})';
+$l['myalerts_setting_tyl'] = 'Receive an alert when someone add thanks/like to my post?';
+$l['myalerts_setting_tyl_like'] = 'Receive an alert when someone likes my post?';
+$l['myalerts_setting_tyl_thanks'] = 'Receive an alert when someone thanks to my post?';
+
+$l['tyl_send_like'] = 'Add like to this post';
+$l['tyl_send_thanks'] = 'Add thanks to this post';
+$l['tyl_remove_like'] = 'Removing like to this post';
+$l['tyl_remove_thanks'] = 'Removing thanks to this post';
