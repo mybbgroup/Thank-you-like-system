777430f1d4717ed957a66fef16fa4edd6af54465
DNT <neogeoman@gmail.com>
2015-05-31 11:46:25 -0500

    Set to stable
    This version was tested and right now after some tests it's stable and must be the latest released version !!!

--- inc/plugins/thankyoulike.php
+++ inc/plugins/thankyoulike.php
@@ -60,7 +60,7 @@ $url_S = '<a href="https://github.com/Cu8eR/thankyou-like-plugin" target="_blank
 		"website"	=> "http://community.mybb.com/thread-169382.html",
 		"author"	=> "- G33K -",
 		"authorsite"	=> "http://community.mybboard.net/user-19236.html",
-		"version"	=> "1.9.4 Beta",
+		"version"	=> "1.9.4",
 		"codename"	=> "thankyoulikesystem",
 		"compatibility"	=> "18*"
     );
