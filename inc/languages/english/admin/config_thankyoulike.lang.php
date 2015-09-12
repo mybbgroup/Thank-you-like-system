94a22c7c6f6715b3ce53c2d5f08e9f61032e01e6
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2015-09-12 09:55:08 +0200

    Follow lang changes for #76
    Follow lang changes for #76

--- inc/languages/english/admin/config_thankyoulike.lang.php
+++ inc/languages/english/admin/config_thankyoulike.lang.php
@@ -11,7 +11,7 @@ $l['tyl_info_desc_recount'] = "Recount Thank You/Likes";
 $l['tyl_info_desc_configsettings'] = "Configure Settings";
 $l['tyl_info_desc_warning'] = "<b>Warning! tyl_unlock file located! Uninstalling will remove ALL the Thank you/Like data from the database. If you would like to preserve the Thank you/Like data while uninstalling then remove the tyl_unlock file from the forum root folder.</b>";
 $l['tyl_info_desc_alerts_error'] = "<b>TYL System is uninstalled or deactivated</b>";
-$l['tyl_info_desc_alerts_integrate'] = "<b>Integrate TYL System with MyAlerts</b>";
+$l['tyl_info_desc_alerts_integrate'] = "<b>Click <u>HERE</u> to integrate TYL System with MyAlerts</b>";
 $l['tyl_info_desc_alerts_integrated'] = "<b>TYL System was integrated with MyAlerts successfully!</b>";
 $l['tyl_info_desc_alerts_registeralerttype'] = "Register a new alert type into MyAlerts";
 
@@ -40,7 +40,7 @@ $l['tyl_exclude_title'] = "Excluded Forums";
 $l['tyl_exclude_desc'] = "Select forums where you do not want the threads and posts to use the Thank you/Like system. (only forums - no categories!)";
 
 $l['tyl_unameformat_title'] = "Format Usernames";
-$l['tyl_unameformat_desc'] = "Do you want to format the usernames in the Thanks/Like list according to their groups?";
+$l['tyl_unameformat_desc'] = "Do you want to format the author's username and usernames in the Thanks/Like list under the post according to their groups?";
 
 $l['tyl_hideforgroups_title'] = "Hide ThankYou/Like Button";
 $l['tyl_hideforgroups_desc'] = "Select User Groups which cannot see Thanks/Like button.";
@@ -55,7 +55,7 @@ $l['tyl_sortorder_title'] = "Sort Order";
 $l['tyl_sortorder_desc'] = "Select the sort order for the Thanks/Like list.";
 
 $l['tyl_collapsible_title'] = "Thanks/Like List Collapsible";
-$l['tyl_collapsible_desc'] = "Do you want the thanks/like list to be collapsible (Show/Hide ability)?";
+$l['tyl_collapsible_desc'] = "Do you want the Thanks/Like list to be collapsible (Show/Hide ability)?";
 
 $l['tyl_colldefault_title'] = "Default Collapsible State";
 $l['tyl_colldefault_desc'] = "If you want the list to be collapsible, what is the default state you want it in when the page loads, open or closed (hidden)?";
