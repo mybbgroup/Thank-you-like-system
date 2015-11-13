adf3b99640cd8cfeab68755808fb5d28d015425c
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2015-11-13 11:51:21 +0100

    Czech part 2

--- inc/languages/czech/thankyoulike.lang.php
+++ inc/languages/czech/thankyoulike.lang.php
@@ -32,15 +32,15 @@ $l['del_ty'] = "Odebrat poděkování";
 $l['add_l'] = "Líbí";
 $l['del_l'] = "Nelíbí";
 
-$l['tyl_total_thanks_given'] = "Total Thanks Given";
-$l['tyl_total_likes_given'] = "Total Likes Given";
-$l['tyl_total_thanks_rcvd'] = "Total Thanks Received";
-$l['tyl_total_likes_rcvd'] = "Total Likes Received";
-$l['tyl_tylpd_percent_total'] = "{1} {2} per day | {3} percent of total {2}";
-$l['tyl_find_ty_threads'] = "Find All Thanked Threads";
-$l['tyl_find_ty_posts'] = "Find All Thanked Posts";
-$l['tyl_find_l_threads'] = "Find All Liked Threads";
-$l['tyl_find_l_posts'] = "Find All Liked Posts";
+$l['tyl_total_thanks_given'] = "Celkově poděkování uděleno";
+$l['tyl_total_likes_given'] = "Celkově líbí uděleno";
+$l['tyl_total_thanks_rcvd'] = "Celkově poděkování obdrženo";
+$l['tyl_total_likes_rcvd'] = "Celkově líbí obdrženo";
+$l['tyl_tylpd_percent_total'] = "{1} {2} za den | {3} procent celkově {2}";
+$l['tyl_find_ty_threads'] = "Najít všechny témata";
+$l['tyl_find_ty_posts'] = "Najít všechny příspěvky";
+$l['tyl_find_l_threads'] = "Najít všechny témata";
+$l['tyl_find_l_posts'] = "Najít všechny příspěvky";
 $l['tyl_find_ty_threads_for'] = "Find All Threads Thanked For";
 $l['tyl_find_ty_posts_for'] = "Find All Posts Thanked For";
 $l['tyl_find_l_threads_for'] = "Find All Threads Liked For";
@@ -50,10 +50,10 @@ $l['tyl_wol_searching'] = "<a href=\"{1}\">Hledání {2}</a>";
 
 $l['tyl_error_invalid_action'] = "You are trying to perform an invalid action.";
 $l['tyl_error_disabled'] = "{1} Feature has been disabled.";
-$l['tyl_error_not_allowed'] = "Not allowed for this post.";
+$l['tyl_error_not_allowed'] = "Není povoleno pro tento příspěvek.";
 $l['tyl_error_excluded'] = "Not allowed for this post because this forum has been excluded.";
-$l['tyl_error_own_post'] = "You can not {1} your own post.";
-$l['tyl_error_already_tyled'] = "You have already {1} this post.";
+$l['tyl_error_own_post'] = "Nemůžete přidat {1} k vašemu vlastnímu příspěvku.";
+$l['tyl_error_already_tyled'] = "Již jste udělili {1} pro tento příspěvek.";
 $l['tyl_error_unknown'] = "There was an unknown error and your {1} was not added.";
 $l['tyl_error_removal_disabled'] = "You can not remove your {1} because removal has been disabled.";
 $l['tyl_error_own_delete'] = "This {1} can not be removed because you are not the user who posted this {1}. You can only delete {1} that YOU added to posts.";
@@ -61,8 +61,8 @@ $l['tyl_error_not_found'] = "The {1} can not be removed because it can not be fo
 $l['tyl_error_threadclosed'] = "You cannot {1} this post because it has been closed.";
 
 $l['tyl_redirect_tyled'] = "Your {1} has been added to the post";
-$l['tyl_redirect_deleted'] = "Your {1} has been removed from the post";
-$l['tyl_redirect_back'] = "<br />You will now be taken back to the post.";
+$l['tyl_redirect_deleted'] = "Váš {1} has been removed from the post";
+$l['tyl_redirect_back'] = "<br />Nyní budete přesměrování zpět na příspěvek.";
 
 
 $l['tyl_alert'] = '{1} thanked/liked your post "{2}" ({3})';
