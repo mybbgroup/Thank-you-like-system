ce13bca7e73aac641a3bf799e3985eb35cddb877
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2015-11-13 12:11:59 +0100

    Last fixes

--- inc/languages/czech/thankyoulike.lang.php
+++ inc/languages/czech/thankyoulike.lang.php
@@ -19,13 +19,13 @@ $l['tyl_liked'] = "Líbí";
 
 $l['tyl_users'] = "uživatelé";
 $l['tyl_user'] = "uživatel";
-$l['tyl_says'] = "says";
-$l['tyl_say'] = "say";
+$l['tyl_says'] = "udelili";
+$l['tyl_say'] = "udělil";
 
 $l['tyl_title_ty'] = "<strong>Následující {1} {2} {3} poděkovali {4} za tento příspěvek:</strong>";
-$l['tyl_title_l'] = "<strong>Následující {1} {2} {3} {4} příspěvek:</strong>";
-$l['tyl_title_collapsed_ty'] = "<strong>{1} {2} {3} Thank You to {4} for this post</strong>";
-$l['tyl_title_collapsed_l'] = "<strong>{1} {2} {3} {4}'s post</strong>";
+$l['tyl_title_l'] = "<strong><strong>Celkem {1} {2} {3}, kteří udělili to se mi líbí {4} za tento příspěvek:</strong>";
+$l['tyl_title_collapsed_ty'] = "<strong>Následující {1} {2} {3} autorovi {4} za tento příspěvek</strong>";
+$l['tyl_title_collapsed_l'] = "<strong>Následující {1} {2} {3} autorovi {4} za tento příspěvek</strong>";
 
 $l['add_ty'] = "Přidat poděkování";
 $l['del_ty'] = "Odebrat poděkování";
@@ -41,38 +41,38 @@ $l['tyl_find_ty_threads'] = "Najít všechny témata";
 $l['tyl_find_ty_posts'] = "Najít všechny příspěvky";
 $l['tyl_find_l_threads'] = "Najít všechny témata";
 $l['tyl_find_l_posts'] = "Najít všechny příspěvky";
-$l['tyl_find_ty_threads_for'] = "Find All Threads Thanked For";
-$l['tyl_find_ty_posts_for'] = "Find All Posts Thanked For";
-$l['tyl_find_l_threads_for'] = "Find All Threads Liked For";
-$l['tyl_find_l_posts_for'] = "Find All Posts Liked For";
+$l['tyl_find_ty_threads_for'] = "Najít všechna témata";
+$l['tyl_find_ty_posts_for'] = "Najít všechny příspěvky";
+$l['tyl_find_l_threads_for'] = "Najít všechna témata";
+$l['tyl_find_l_posts_for'] = "Najít všechny příspěvky";
 
 $l['tyl_wol_searching'] = "<a href=\"{1}\">Hledání {2}</a>";
 
-$l['tyl_error_invalid_action'] = "You are trying to perform an invalid action.";
-$l['tyl_error_disabled'] = "{1} Feature has been disabled.";
+$l['tyl_error_invalid_action'] = "Snažíte se provést neplatnou akci.";
+$l['tyl_error_disabled'] = "{1} funkce byla vypnuta.";
 $l['tyl_error_not_allowed'] = "Není povoleno pro tento příspěvek.";
-$l['tyl_error_excluded'] = "Not allowed for this post because this forum has been excluded.";
+$l['tyl_error_excluded'] = "Není povoleno pro tento příspěvek, protože toto fórum bylo vyřazeno pro možnost přidání poděkování/to se mi líbí administrátorem.";
 $l['tyl_error_own_post'] = "Nemůžete přidat {1} k vašemu vlastnímu příspěvku.";
 $l['tyl_error_already_tyled'] = "Již jste udělili {1} pro tento příspěvek.";
-$l['tyl_error_unknown'] = "There was an unknown error and your {1} was not added.";
-$l['tyl_error_removal_disabled'] = "You can not remove your {1} because removal has been disabled.";
-$l['tyl_error_own_delete'] = "This {1} can not be removed because you are not the user who posted this {1}. You can only delete {1} that YOU added to posts.";
-$l['tyl_error_not_found'] = "The {1} can not be removed because it can not be found. Its possible that it has already been removed.";
-$l['tyl_error_threadclosed'] = "You cannot {1} this post because it has been closed.";
+$l['tyl_error_unknown'] = "Nastala neočekávaná chyba a vaše {1} nebylo přidáno.";
+$l['tyl_error_removal_disabled'] = "Nemůžete odebrat vaše {1}, protože možnost odebrání byla vypnuta.";
+$l['tyl_error_own_delete'] = "Toto {1} nemůže být odebráno, protože nejste uživatel, který vložil toto {1}. Můžete pouze odebrat {1}, které jste VY přidali k příspěvku.";
+$l['tyl_error_not_found'] = "{1} nemůže být odebráno, protože již neexistuje. Je možné, že již bylo odebráno dříve.";
+$l['tyl_error_threadclosed'] = "Nemůžete přidat {1} k tomuto příspěvku, protože toto téma, ve kterém se nachazí příspěvek bylo uzavřeno.";
 
-$l['tyl_redirect_tyled'] = "Your {1} has been added to the post";
-$l['tyl_redirect_deleted'] = "Váš {1} has been removed from the post";
+$l['tyl_redirect_tyled'] = "Vaše {1} bylo přidáno k příspěvku";
+$l['tyl_redirect_deleted'] = "Vaše {1} bylo odebráno z příspěvku";
 $l['tyl_redirect_back'] = "<br />Nyní budete přesměrování zpět na příspěvek.";
 
 
-$l['tyl_alert'] = '{1} thanked/liked your post "{2}" ({3})';
-$l['tyl_alert_like'] = '{1} liked your post "{2}" ({3})';
-$l['tyl_alert_thanks'] = '{1} thanked your post "{2}" ({3})';
-$l['myalerts_setting_tyl'] = 'Receive an alert when someone add thanks/like to my post?';
-$l['myalerts_setting_tyl_like'] = 'Receive an alert when someone likes my post?';
-$l['myalerts_setting_tyl_thanks'] = 'Receive an alert when someone thanks to my post?';
+$l['tyl_alert'] = '{1} přidal poděkování/to se mi líbí k vašemu příspěvku <b>"{2}"</b>';
+$l['tyl_alert_like'] = '{1} přidal to se mi líbí k vašemu příspěvku <b>"{2}"</b>';
+$l['tyl_alert_thanks'] = '{1} přidal poděkování k vašemu příspěvku <b>"{2}"</b>';
+$l['myalerts_setting_tyl'] = 'Dostávat upozornění, pokud někdo přidá poděkování/to se mi líbí k vašemu příspěvku?';
+$l['myalerts_setting_tyl_like'] = 'Dostávat upozornění, pokud někdo přidá to se mi líbí k vašemu příspěvku?';
+$l['myalerts_setting_tyl_thanks'] = 'Dostávat upozornění, pokud někdo přidá poděkování k vašemu příspěvku?';
 
-$l['tyl_send_like'] = 'Add like to this post';
-$l['tyl_send_thanks'] = 'Add thanks to this post';
-$l['tyl_remove_like'] = 'Removing like to this post';
-$l['tyl_remove_thanks'] = 'Removing thanks to this post';
+$l['tyl_send_like'] = 'To se mi líbí úspěšně přidáno';
+$l['tyl_send_thanks'] = 'Poděkování úspěšně přidáno';
+$l['tyl_remove_like'] = 'To se mi líbí odebráno z příspěvku';
+$l['tyl_remove_thanks'] = 'Poděkování odebráno z příspěvku';
