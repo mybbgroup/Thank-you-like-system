1b6e1d25e044ba073d467259e04c243807f718cc
Sven <svp1702@freenet.de>
2015-11-14 20:06:55 +0100

    Fixes #90 Recounting thanks/likes in ACP

--- inc/languages/english/admin/tools_thankyoulike_recount.lang.php
+++ inc/languages/english/admin/tools_thankyoulike_recount.lang.php
@@ -11,3 +11,4 @@ $l['tyl_data_per_page'] = "Data Entries Per Page";
 $l['tyl_recount_do_desc'] = "When this is run, the Thank You/Like count for each user and post will be updated to reflect its current live value based on the data in the database.";
 $l['tyl_success_thankyoulike_rebuilt'] = "The Thank You/Likes have been recounted successfully.";
 $l['tyl_confirm_proceed_rebuild'] = "Click \"Proceed\" to continue the Thank You/Like recount process.";
+$l['tyl_automatically_redirecting'] = "Automatically Redirecting...";
