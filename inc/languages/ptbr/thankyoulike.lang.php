67dc277aa403b5b3f51545daf4681dd6aa4476dd
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2015-11-14 21:07:47 +0100

    Add ptbr lang - not completed translation

---
+++ inc/languages/ptbr/thankyoulike.lang.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Thank You / Like System Pacote de Língua Brasileira
+ * 
+ * $Id: thankyoulike.lang.php 31 2011-04-21 23:12:00Z - G33K - $
+ */
+$l['tyl_thankyou'] = "Obrigado";
+$l['tyl_thanked'] = "Agradecido";
+$l['tyl_thanks'] = "Agradecidos";
+$l['tyl_thanks_given'] = "Agradecimentos dados";
+$l['tyl_thanks_rcvd'] = "Agradecimentos recebidos";
+$l['tyl_thanks_rcvd_bit'] = "{1} em {2} mensagens";
+$l['tyl_like'] = "Curtit";
+$l['tyl_likes'] = "Curtidas";
+$l['tyl_likes_given'] = "Curtidas Dadas";
+$l['tyl_likes_rcvd'] = "Curtidas Recebidas";
+$l['tyl_likes_rcvd_bit'] = "{1} em {2} mensagens";
+$l['tyl_liked'] = "Curtidas";
+$l['tyl_users'] = "usiários";
+$l['tyl_user'] = "usuário";
+$l['tyl_says'] = "Ditos";
+$l['tyl_say'] = "Dito";
+$l['tyl_title_ty'] = "<strong>The following {1} {2} {3} Thank You to {4} for this post:</strong>";
+$l['tyl_title_l'] = "<strong>The following {1} {2} {3} {4}'s post:</strong>";
+$l['tyl_title_collapsed_ty'] = "<strong>{1} {2} {3} Thank You to {4} for this post</strong>";
+$l['tyl_title_collapsed_l'] = "<strong>{1} {2} {3} {4}'s post</strong>";
+$l['add_ty'] = "Add Thank You";
+$l['del_ty'] = "Remove Thank You";
+$l['add_l'] = "Like Post";
+$l['del_l'] = "Unlike Post";
+$l['tyl_total_thanks_given'] = "Total Thanks Given";
+$l['tyl_total_likes_given'] = "Total Likes Given";
+$l['tyl_total_thanks_rcvd'] = "Total Thanks Received";
+$l['tyl_total_likes_rcvd'] = "Total Likes Received";
+$l['tyl_tylpd_percent_total'] = "{1} {2} per day | {3} percent of total {2}";
+$l['tyl_find_ty_threads'] = "Find All Thanked Threads";
+$l['tyl_find_ty_posts'] = "Find All Thanked Posts";
+$l['tyl_find_l_threads'] = "Find All Liked Threads";
+$l['tyl_find_l_posts'] = "Find All Liked Posts";
+$l['tyl_find_ty_threads_for'] = "Find All Threads Thanked For";
+$l['tyl_find_ty_posts_for'] = "Find All Posts Thanked For";
+$l['tyl_find_l_threads_for'] = "Find All Threads Liked For";
+$l['tyl_find_l_posts_for'] = "Find All Posts Liked For";
+$l['tyl_wol_searching'] = "<a href=\"{1}\">Searching {2}</a>";
+$l['tyl_error_invalid_action'] = "You are trying to perform an invalid action.";
+$l['tyl_error_disabled'] = "{1} Feature has been disabled.";
+$l['tyl_error_not_allowed'] = "Not allowed for this post.";
+$l['tyl_error_excluded'] = "Not allowed for this post because this forum has been excluded.";
+$l['tyl_error_own_post'] = "You can not {1} your own post.";
+$l['tyl_error_already_tyled'] = "You have already {1} this post.";
+$l['tyl_error_unknown'] = "There was an unknown error and your {1} was not added.";
+$l['tyl_error_removal_disabled'] = "You can not remove your {1} because removal has been disabled.";
+$l['tyl_error_own_delete'] = "This {1} can not be removed because you are not the user who posted this {1}. You can only delete {1} that YOU added to posts.";
+$l['tyl_error_not_found'] = "The {1} can not be removed because it can not be found. Its possible that it has already been removed.";
+$l['tyl_error_threadclosed'] = "You cannot {1} this post because it has been closed.";
+$l['tyl_redirect_tyled'] = "Your {1} has been added to the post";
+$l['tyl_redirect_deleted'] = "Your {1} has been removed from the post";
+$l['tyl_redirect_back'] = "<br />You will now be taken back to the post.";
+$l['tyl_alert'] = '{1} agradecidos/curtidos nas mensagens "{2}" ({3})';
+$l['myalerts_setting_tyl'] = 'Receber um alerta quando alguém agrade/curte minhas mensagens?';
