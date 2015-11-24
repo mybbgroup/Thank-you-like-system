723bcedc3209720622c9040e1c2e95d430e8ea08
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2015-11-24 11:36:55 +0100

    Preparing for v1.9.10

--- inc/plugins/thankyoulike.php
+++ inc/plugins/thankyoulike.php
@@ -61,7 +61,7 @@ $url_S = '<a href="https://github.com/Cu8eR/thankyou-like-plugin" target="_blank
 		"website"	=> "http://community.mybb.com/thread-169382.html",
 		"author"	=> "- G33K -",
 		"authorsite"	=> "http://community.mybboard.net/user-19236.html",
-		"version"	=> "1.9.9",
+		"version"	=> "1.9.10",
 		"codename"	=> "thankyoulikesystem",
 		"compatibility"	=> "18*"
     );
