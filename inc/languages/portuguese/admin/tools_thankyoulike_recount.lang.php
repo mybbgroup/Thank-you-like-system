a728f0065ab14e9b570db0b6fb31a89ca29f7250
Filipe Prata <prata_filipe2@hotmail.com>
2016-02-10 10:54:48 +0000

    Create tools_thankyoulike_recount.lang.php

---
+++ inc/languages/portuguese/admin/tools_thankyoulike_recount.lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Thank You / Like System Recount portuguese Language Pack by Blackbeard (from mybb community)
+ * 
+ */
+
+$l['tyl_recount'] = "Recontagem TYL";
+$l['tyl_recount_desc'] = "Aqui pode recontar a data do sistema TYL para corrigir algum problema de sincronização no seu fórum.";
+$l['tyl_name'] = "Nome";
+$l['tyl_data_per_page'] = "Entrada de Dada por página";
+$l['tyl_recount_do_desc'] = "Quando isto for corrido, a contagem de obrigados/gostos de cada membro e mensagens será actualizada para estar de acordo com o seu valor real que se encontra na base de dados.";
+$l['tyl_success_thankyoulike_rebuilt'] = "A recontagem foi efectuada com sucesso.";
+$l['tyl_confirm_proceed_rebuild'] = "Clique \"Proceed\" para continuar com o processo de recontagem.";
+$l['tyl_automatically_redirecting'] = "Redirecionando automaticamente...";
