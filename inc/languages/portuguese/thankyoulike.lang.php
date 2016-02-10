f3b6691cf00111aa54627d19b7d88ab0eaf55c11
Filipe Prata <prata_filipe2@hotmail.com>
2016-02-10 10:53:23 +0000

    Create thankyoulike.lang.php

---
+++ inc/languages/portuguese/thankyoulike.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Thank You / Like System Portuguese Language Pack tanslated by Blackbeard (from mybb community)
+ * 
+ */
+
+$l['tyl_thankyou'] = "Obrigado";
+$l['tyl_thanked'] = "Obrigado";
+$l['tyl_thanks'] = "Obrigados";
+$l['tyl_thanks_given'] = "Obrigados dados";
+$l['tyl_thanks_rcvd'] = "Obrigados recebidos";
+$l['tyl_thanks_rcvd_bit'] = "{1} em {2} mensagens";
+$l['tyl_like'] = "Gosto";
+$l['tyl_likes'] = "Gostos";
+$l['tyl_likes_given'] = "Gostos dados";
+$l['tyl_likes_rcvd'] = "Gostos recebidos";
+$l['tyl_likes_rcvd_bit'] = "{1} em {2} mensagens";
+$l['tyl_liked'] = "Gostei";
+
+$l['tyl_users'] = "membros";
+$l['tyl_user'] = "membro";
+$l['tyl_says'] = "dizem";
+$l['tyl_say'] = "diz";
+
+$l['tyl_title_ty'] = "<strong>Os seguintes {1} {2} {3} agradeceram a {4} por esta mensagem:</strong>";
+$l['tyl_title_l'] = "<strong>O seguinte {1} {2} {3} {4} mensagem:</strong>";
+$l['tyl_title_collapsed_ty'] = "<strong>{1} {2} {3} agradecem a {4} por esta mensagem</strong>";
+$l['tyl_title_collapsed_l'] = "<strong>{1} {2} {3} {4} mensagem</strong>";
+
+$l['add_ty'] = "Agradecer";
+$l['del_ty'] = "Remover o obrigado";
+$l['add_l'] = "Gostar desta mensagem";
+$l['del_l'] = "Não gostar desta mensagem";
+
+$l['tyl_total_thanks_given'] = "Total de agradecimentos dados";
+$l['tyl_total_likes_given'] = "Total de gostos dados";
+$l['tyl_total_thanks_rcvd'] = "Total de agradecimentos recebidos";
+$l['tyl_total_likes_rcvd'] = "Total de gostos recebidos";
+$l['tyl_tylpd_percent_total'] = "{1} {2} por dia | {3} percentagem do total {2}";
+$l['tyl_find_ty_threads'] = "Encontrar todos os tópicos que agradeceu";
+$l['tyl_find_ty_posts'] = "Encontrar todas as mensagens que agradeceu";
+$l['tyl_find_l_threads'] = "Encontrar todos os tópicos que gostou";
+$l['tyl_find_l_posts'] = "Encontrar todas as mensagens que gostou";
+$l['tyl_find_ty_threads_for'] = "Encontrar todos os tópicos que foi agradecido";
+$l['tyl_find_ty_posts_for'] = "Encontrar todas as mensagens que foi agradecido";
+$l['tyl_find_l_threads_for'] = "Encontrar todos os tópicos que os membros gostaram";
+$l['tyl_find_l_posts_for'] = "Encontrar todas as mensagens que os membros gostaram";
+
+$l['tyl_wol_searching'] = "<a href=\"{1}\">A procurar {2}</a>";
+
+$l['tyl_error_invalid_action'] = "Está a tentar realizar uma acção que é inválida.";
+$l['tyl_error_disabled'] = "{1} funcionalidade foi desactivada.";
+$l['tyl_error_not_allowed'] = "Não é permitido para esta mensagem.";
+$l['tyl_error_excluded'] = "Não é permitido para esta mensagem porque esta secção foi excluída.";
+$l['tyl_error_own_post'] = "Não é possível {1} da sua própria mensagem.";
+$l['tyl_error_already_tyled'] = "Já tem {1} nesta mensagem.";
+$l['tyl_error_unknown'] = "Ocorreu um erro desconhecido e o seu {1} não foi adicionado.";
+$l['tyl_error_removal_disabled'] = "Não pode remover o seu {1} porque a função de poder remover foi desactivada.";
+$l['tyl_error_own_delete'] = "Este {1} não pode ser removido porque não é o membro que postou isto {1}. Só é permitido apagar {1} que o membro adicionou à mensagem.";
+$l['tyl_error_not_found'] = "Este {1} não pode ser removido porque não foi encontrado. É possível que já se encontre removido.";
+$l['tyl_error_threadclosed'] = "Não pode {1} desta mensagem porque o tópico já foi fechado.";
+
+$l['tyl_redirect_tyled'] = "O seu {1} foi adicionado à mensagem";
+$l['tyl_redirect_deleted'] = "O seu {1} foi removido desta mensagem";
+$l['tyl_redirect_back'] = "<br />Vai ser redireccionado agora para a mensagem.";
+
+
+$l['tyl_alert'] = '{1} agradeceu/gostou da sua mensagem <b>"{2}"</b>.';
+$l['tyl_alert_like'] = '{1} gostou da sua mensagem <b>"{2}"</b>.';
+$l['tyl_alert_thanks'] = '{1} agradeceu a sua mensagem <b>"{2}"</b>.';
+$l['myalerts_setting_tyl'] = 'Receber um alerta quando um membro agradece/gosta da minha mensagem?';
+$l['myalerts_setting_tyl_like'] = 'Receber um alerta quando um membro gosta da minha mensagem?';
+$l['myalerts_setting_tyl_thanks'] = 'Receber um alerta quando um membro agradece a minha mensagem?';
+
+$l['tyl_send_like'] = 'Está a gostar desta mensagem';
+$l['tyl_send_thanks'] = 'Está a agradecer esta mensagem';
+$l['tyl_remove_like'] = 'Está a remover o gosto desta mensagem';
+$l['tyl_remove_thanks'] = 'Está a remover o agradecimento desta mensagem';
