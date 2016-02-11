d32eaf55baca657dd79f0ddda787f50e2ee642d4
Filipe Prata <prata_filipe2@hotmail.com>
2016-02-11 13:02:31 +0000

    Create config_thankyoulike.lang.php

---
+++ inc/languages/portuguese/admin/config_thankyoulike.lang.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Thank You / Like Config System Language Pack Portuguese by Blackbeard (from mybb community))
+ * 
+ */
+
+$l['tyl_info_title'] = "Sistema Obrigado/Gosto";
+$l['tyl_info_desc'] = "Adiciona uma opção para os membros agradecerem o membro que colocou a mensagem ou gostarem da mensagem.";
+$l['tyl_info_desc_url'] ="<br />*Editado para o MyBB 1.8 por: {1}<br />*Mantido por: {2}, {3} e {4}<br />*Fontes: {5}";
+$l['tyl_info_desc_recount'] = "Recontar Obrigados/Gostos";
+$l['tyl_info_desc_configsettings'] = "Configurar as definições";
+$l['tyl_info_desc_alerts_error'] = "<b>O sistema TYL está desinstalado ou desactivado</b>";
+$l['tyl_info_desc_alerts_integrate'] = "<b>Clique <u>AQUI</u> para integrar o sistema TYL com o MyAlerts</b>";
+$l['tyl_info_desc_alerts_integrated'] = "<b>O sistema TYL e o MyAlerts foram integrados com sucesso!</b>";
+$l['tyl_info_desc_alerts_registeralerttype'] = "Registar um novo tipo de alerta para o MyAlerts";
+
+$l['tyl_title'] = "Sistema Obrigado/Gosto";
+$l['tyl_desc'] = "Definições para costumizar o sistema do plugin Obrigado/Gosto";
+
+$l['tyl_enabled_title'] = "Activar/Desactivar";
+$l['tyl_enabled_desc'] = "Activar/Desactivar o sistema Obrigado/Gosto";
+
+$l['tyl_thankslike_title'] = "Obrigado ou Gosto";
+$l['tyl_thankslike_desc'] = "Escolha se quer utilizar o sistema de agradecimentos (obrigados) ou o sistema de gostos.";
+
+$l['tyl_firstall_title'] = "Apenas na primeira mensagem ou em todas";
+$l['tyl_firstall_desc'] = "Quer que os agradecimentos ou os gostos sejam dados apenas na primeira mensagem de um tópico ou em todas as mensagens de um tópico?";
+
+$l['tyl_firstalloverwrite_title'] = "Opção especial para mostrar os butões dos agradecimentos/gostos em TODAS as mensagens";
+$l['tyl_firstalloverwrite_desc'] = "Sobreescreva a opção acima selecionada Todos em certas secções (escolha apenas secções - <strong>não escolha categorias</strong>!)";
+
+$l['tyl_removing_title'] = "Permitir apagar";
+$l['tyl_removing_desc'] = "Quer permitir a função de os membros poderem remover os seus agradecimentos/gostos de uma mensagem onde já tinha agradecido/gostado?";
+
+$l['tyl_closedthreads_title'] = "Permitir em tópicos fechados";
+$l['tyl_closedthreads_desc'] = "Quer permitir a função de os membros poderem agradecer/gostar em tópicos fechados?";
+
+$l['tyl_exclude_title'] = "Secções excluídas";
+$l['tyl_exclude_desc'] = "Seleccione as secções que não quer que os tópicos e as mensagens usem o sistema de Obrigados/Gostos. (apenas secções - não escolha categorias!)";
+
+$l['tyl_unameformat_title'] = "Formatar os nomes dos membros";
+$l['tyl_unameformat_desc'] = "Quer formatar o nome do autor e dos membros na lista dos Obrigados/Gostos por baixo da mensagem de acordo com os seus grupos?";
+
+$l['tyl_hideforgroups_title'] = "Esconder o botão Obrigado/Gosto";
+$l['tyl_hideforgroups_desc'] = "Seleccione os grupos que não podem ver o botão Obrigado/Gosto.";
+
+$l['tyl_showdt_title'] = "Mostrar data/tempo";
+$l['tyl_showdt_desc'] = "Quer que se mostre a Data/Tempo dos Obrigados/Gostos quando eles foram recebidos na sua devida lista?";
+
+$l['tyl_dtformat_title'] = "Formato Data/Temopo";
+$l['tyl_dtformat_desc'] = "Escolha o formato que quer utilizar na sua lista.<br />Format is same as the one used by PHP\'s date() function.<br />Example format: m-d-Y h:i A &lt;&lt;will show&gt;&gt; 12-31-2009 12:01 PM";
+
+$l['tyl_sortorder_title'] = "Ordem";
+$l['tyl_sortorder_desc'] = "Escolha a ordem da lista dos Obrigados/Gostos.";
+
+$l['tyl_collapsible_title'] = "Opção de esconder a lista de Obrigados/Gostos";
+$l['tyl_collapsible_desc'] = "Quer que a lista de Obrigados/Gostos possa ser escondida pelos membros ( Mostrar/Esconder)?";
+
+$l['tyl_colldefault_title'] = "Opção de esconder padrão";
+$l['tyl_colldefault_desc'] = "Se deseja que a lista possa ser escondida, qual é a escolha padrão que quer imposta cada vez que a página recarrega, aberto ou fechado (escondido)?";
+
+$l['tyl_hidelistforgroups_title'] = "Esconder a lista TYL";
+$l['tyl_hidelistforgroups_desc'] = "Escolha os grupos que não podem ver a lista TYL.";
+
+$l['tyl_displaygrowl_title'] = "Mostrar a notificação popup de adicionar/remover";
+$l['tyl_displaygrowl_desc'] = "Escolha ON para permitir a janela popup de notificação AJAX.";
+
+$l['tyl_uninstall'] = 'Sistema TYL - Desinstalar';
+$l['tyl_uninstall_message'] = 'Deseja apagar todas as entradas de data do plugin na sua base de dados?';
+
+$l['setting_thankyoulike_promotion'] = 'Contagem TYL';
+$l['setting_thankyoulike_promotion_desc'] = 'Introduza o número de Obrigados/Gostos necessários. A contagem dos agradecimentos/gostos tem de ser selecionada como um valor necessário para ser incluído. Selecione o tipo de comparação para os Agradecimentos/Gostos';
