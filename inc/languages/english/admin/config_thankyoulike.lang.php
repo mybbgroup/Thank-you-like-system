3c5c2642cbb09ee47dcb73cc152994c341273b04
Eldenroot <778890+Eldenroot@users.noreply.github.com>
2018-06-03 10:08:42 +0200

    Update language files - English ACP

--- inc/languages/english/admin/config_thankyoulike.lang.php
+++ inc/languages/english/admin/config_thankyoulike.lang.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Thank You / Like System Config Language Pack English 
+ * Thank You / Like System Config Language Pack - English 
  * 
  */
 
@@ -15,105 +15,105 @@ $l['tyl_info_desc_alerts_integrated'] = "<b>Thank You/Like System and MyAlerts w
 $l['tyl_info_desc_alerts_registeralerttype'] = "Register a new alert type into MyAlerts";
 
 $l['tyl_title'] = "Thank You/Like System";
-$l['tyl_desc'] = "Settings to customize the Thank You/Like System Plugin";
+$l['tyl_desc'] = "Settings to customize Thank You/Like System plugin.";
 
 $l['tyl_enabled_title'] = "Enable/Disable";
-$l['tyl_enabled_desc'] = "Enable/Disable the Thank You/Like System";
+$l['tyl_enabled_desc'] = "Enable/Disable the Thank You/Like System.";
 
 $l['tyl_thankslike_title'] = "Thank You or Like";
-$l['tyl_thankslike_desc'] = "Choose if you want to use the Thank You or the Like System.";
-$l['tyl_thankslike_op_1'] = "Use Thank You";
-$l['tyl_thankslike_op_2'] = "Use Like";
+$l['tyl_thankslike_desc'] = "Choose if you want to use Thank You or Like System.";
+$l['tyl_thankslike_op_1'] = "Use Thank You system";
+$l['tyl_thankslike_op_2'] = "Use Like system";
 
 $l['tyl_firstall_title'] = "First Post only or All";
 $l['tyl_firstall_desc'] = "Do you want the Thanks/Likes to be given on the first post of a thread only or on all the posts of a thread?";
-$l['tyl_firstall_op_1'] = "First Post Only";
-$l['tyl_firstall_op_2'] = "All Posts";
+$l['tyl_firstall_op_1'] = "First post only";
+$l['tyl_firstall_op_2'] = "All posts";
 
-$l['tyl_firstalloverwrite_title'] = "Special option for Display Thank You/Like buttons in ALL Posts";
-$l['tyl_firstalloverwrite_desc'] = "Overwrite the above selected option All in certain forums (choose only forums - no categories!).";
+$l['tyl_firstalloverwrite_title'] = "Special option for display Thank You/Like buttons in ALL posts";
+$l['tyl_firstalloverwrite_desc'] = "Overwrite the above selected option All in certain forums (choose only forums, no categories!).";
 
-$l['tyl_removing_title'] = "Allow Removing";
+$l['tyl_removing_title'] = "Allow removing";
 $l['tyl_removing_desc'] = "Do you want to allow the removing of Thank/Like from a post already Thanked/Liked?";
 
-$l['tyl_tylownposts_title'] = "Allow Giving Thanks/Likes to own Posts";
+$l['tyl_tylownposts_title'] = "Allow giving Thanks/Likes to own posts";
 $l['tyl_tylownposts_desc'] = "Do you want to allow users to give Thanks/Likes to own posts?";
 
-$l['tyl_remowntylfroms_title'] = "Remove Own Given Thanks/Likes From Search Results";
-$l['tyl_remowntylfroms_desc'] = "Choose YES to hide own given Thanks/Likes on search list";
+$l['tyl_remowntylfroms_title'] = "Remove own given Thanks/Likes from search results";
+$l['tyl_remowntylfroms_desc'] = "Choose YES to hide own given Thanks/Likes on search list.";
 
-$l['tyl_remowntylfromc_title'] = "Remove Numbers of Own Given Thanks/Likes in Counters";
-$l['tyl_remowntylfromc_desc'] = "Choose YES to hide own given Thanks/Likes in post and member profile counters";
+$l['tyl_remowntylfromc_title'] = "Remove numbers of own given Thanks/Likes in counters";
+$l['tyl_remowntylfromc_desc'] = "Choose YES to hide own given Thanks/Likes in post and member profile counters.";
 
 $l['tyl_reputation_add_title'] = "Reputation points for given Thanks/Likes";
-$l['tyl_reputation_add_desc'] = "Do you want to add reputation points for a thank or like?";
+$l['tyl_reputation_add_desc'] = "Do you want to add reputation points for given Thanks/Likes?";
 
 $l['tyl_reputation_add_reppoints_title'] = "Number of reputation points to add";
-$l['tyl_reputation_add_reppoints_desc'] = "Enter the number of reputation points you want to add for a thank or like (default: 1)";
+$l['tyl_reputation_add_reppoints_desc'] = "Enter the number of reputation points you want to add for a Thank/Like (default value: 1).";
 
-$l['tyl_reputation_add_repcomment_title'] = "Comment of given reputation.";
-$l['tyl_reputation_add_repcomment_desc'] = "Enter a comment which will be displayed on thanked/liked users reputation overview. (default: empty)";
+$l['tyl_reputation_add_repcomment_title'] = "Comment of given reputation";
+$l['tyl_reputation_add_repcomment_desc'] = "Enter a text which will be displayed on Thanked/Liked users reputation overview (default value: empty).";
 
-$l['tyl_closedthreads_title'] = "Allow in Closed Threads";
-$l['tyl_closedthreads_desc'] = "Do you want to allow to give Thank/Like in closed threads?";
+$l['tyl_closedthreads_title'] = "Allow in closed threads";
+$l['tyl_closedthreads_desc'] = "Do you want to allow to give Thanks/Likes in closed threads?";
 
-$l['tyl_exclude_title'] = "Excluded Forums";
+$l['tyl_exclude_title'] = "Excluded forums";
 $l['tyl_exclude_desc'] = "Select forums where you do not want the threads and posts to use the Thank you/Like system (only forums - no categories!).";
 
-$l['tyl_unameformat_title'] = "Format Usernames";
+$l['tyl_unameformat_title'] = "Format usernames";
 $l['tyl_unameformat_desc'] = "Do you want to format the author's username and usernames in the Thank/Like list under the post according to their groups?";
 
-$l['tyl_hideforgroups_title'] = "Hide Thank/Like Button";
-$l['tyl_hideforgroups_desc'] = "Select User Groups which cannot see Thank/Like button.";
+$l['tyl_hideforgroups_title'] = "Hide Thank/Like button";
+$l['tyl_hideforgroups_desc'] = "Select usergroups which cannot see Thank/Like button.";
 
 $l['tyl_showdt_title'] = "Show Date/Time";
-$l['tyl_showdt_desc'] = "Do you want to show the Date/Time the Thank/Like was received in the Thank/Like list?";
-$l['tyl_showdt_op_1'] = "Not Display";
+$l['tyl_showdt_desc'] = "Do you want to show Date/Time for Thank/Like in the list?";
+$l['tyl_showdt_op_1'] = "Not display";
 $l['tyl_showdt_op_2'] = "Display next to user name";
 $l['tyl_showdt_op_3'] = "Display on mouse hover over username";
 
-$l['tyl_dtformat_title'] = "Date/Time Format";
-$l['tyl_dtformat_desc'] = "Set the format you want to use to show the Date/Time in the thank/like list.<br />Format is same as the one used by PHP\'s date() function.<br />Example format: m-d-Y h:i A &lt;&lt;will show&gt;&gt; 12-31-2009 12:01 PM";
+$l['tyl_dtformat_title'] = "Date/Time format";
+$l['tyl_dtformat_desc'] = "Set the format you want to use to show the Date/Time in the thanks/likes list.<br />Format is same as the one used by PHP\'s date() function.<br />Example format: m-d-Y h:i A &lt;&lt;will show&gt;&gt; 12-31-2009 12:01 PM";
 
 $l['tyl_sortorder_title'] = "Sort Order";
 $l['tyl_sortorder_desc'] = "Select the sort order for the Thank/Like list.";
-$l['tyl_sortorder_op_1'] = "Username Ascending";
-$l['tyl_sortorder_op_2'] = "Username Descending";
-$l['tyl_sortorder_op_3'] = "Date/Time Added Ascending";
-$l['tyl_sortorder_op_4'] = "Date/Time Added Descending";
+$l['tyl_sortorder_op_1'] = "Username ascending";
+$l['tyl_sortorder_op_2'] = "Username descending";
+$l['tyl_sortorder_op_3'] = "Date/Time added ascending";
+$l['tyl_sortorder_op_4'] = "Date/Time added dDescending";
 
-$l['tyl_collapsible_title'] = "Thank/Like List Collapsible";
-$l['tyl_collapsible_desc'] = "Do you want the Thank/Like list to be collapsible (Show/Hide ability)?";
+$l['tyl_collapsible_title'] = "Thank/Like list collapsible";
+$l['tyl_collapsible_desc'] = "Do you want the Thank/Like list to be collapsible (show/hide ability)?";
 
-$l['tyl_colldefault_title'] = "Default Collapsible State";
+$l['tyl_colldefault_title'] = "Default collapsible state";
 $l['tyl_colldefault_desc'] = "If you want the list to be collapsible, what is the default state you want it in when the page loads, open or closed (hidden)?";
-$l['tyl_colldefault_op_1'] = "List Shown";
-$l['tyl_colldefault_op_2'] = "List Hidden (Collapsed)";
+$l['tyl_colldefault_op_1'] = "List shown";
+$l['tyl_colldefault_op_2'] = "List hidden (collapsed)";
 
-$l['tyl_hidelistforgroups_title'] = "Hide Thank You/Like List";
-$l['tyl_hidelistforgroups_desc'] = "Select User Groups which cannot see Thank You/Like list.";
+$l['tyl_hidelistforgroups_title'] = "Hide Thank You/Like list";
+$l['tyl_hidelistforgroups_desc'] = "Select usergroups which cannot see Thank You/Like list.";
 
-$l['tyl_displaygrowl_title'] = "Display Add/Remove Notification Popup";
+$l['tyl_displaygrowl_title'] = "Display Add/Remove notification popup";
 $l['tyl_displaygrowl_desc'] = "Choose ON to enable the Ajax notification popup window.";
 
-$l['tyl_limits_title'] = "Enable User group based Thank You/Likes Limits?";
-$l['tyl_limits_desc'] = "Choose Yes to enable this feature and check the usergroup settings.";
-$l['tyl_highlight_popular_posts_title'] = "Highlight Posts with X Numbers of Thank You/Likes?";
-$l['tyl_highlight_popular_posts_desc'] = "Choose Yes to enable this feature and define your style of CSS class <strong>.popular_post</strong> in thankyoulike.css (in your theme).";
-$l['tyl_highlight_popular_posts_count_title'] = "Numbers of Thank You/Likes for Highlight Posts";
+$l['tyl_limits_title'] = "Enable usergroup based Thank You/Likes limits?";
+$l['tyl_limits_desc'] = "Choose YES to enable this feature and check the usergroup settings.";
+$l['tyl_highlight_popular_posts_title'] = "Highlight posts with X numbers of Thank You/Likes?";
+$l['tyl_highlight_popular_posts_desc'] = "Choose YES to enable this feature and define your style of CSS class <strong>.popular_post</strong> in thankyoulike.css (in your theme).";
+$l['tyl_highlight_popular_posts_count_title'] = "Numbers of Thank You/Likes for highlight posts";
 $l['tyl_highlight_popular_posts_count_desc'] = "Set the number of Thank You/Likes the post will be highlighted.";
 
-$l['tyl_limits_permissions_system'] = "Thanks/Likes Limits";
-$l['tyl_limits_permissions_title'] = "Maximum Thanks/Likes Allowed Per 24 Hours:";
+$l['tyl_limits_permissions_system'] = "Thanks/Likes limits";
+$l['tyl_limits_permissions_title'] = "Maximum Thanks/Likes allowed per 24 hours:";
 $l['tyl_limits_permissions_desc'] = "Here you can enter the maximum number of thanks/likes that users in this group can give per day (24 hours). To allow unlimited reputations per hour, enter 0.";
 
 $l['tyl_uninstall'] = 'Thank You/Like System - Uninstallation';
 $l['tyl_uninstall_message'] = 'Do you wish to drop all plugin data entries from the database?';
 
-$l['setting_thankyoulike_promotion_rcv'] = 'Thank You/Like Received';
+$l['setting_thankyoulike_promotion_rcv'] = 'Thank You/Like received';
 $l['setting_thankyoulike_promotion_rcv_desc'] = 'Enter the number of received Thanks/Likes required. Thanks/Likes count must be selected as a required value for this to be included. Select the type of comparison for Thanks/Likes.';
 
-$l['setting_thankyoulike_promotion_gvn'] = 'Thank You/Like Given';
+$l['setting_thankyoulike_promotion_gvn'] = 'Thank You/Like given';
 $l['setting_thankyoulike_promotion_gvn_desc'] = 'Enter the number of given Thanks/Likes required. Thanks/Likes count must be selected as a required value for this to be included. Select the type of comparison for Thanks/Likes.';
 
 $l['tyl_recount'] = "Recount Thank You/Like";
