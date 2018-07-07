b1eb34fa516f81cea585abe4cf96a5645daf3211
Laird <lairdshaw@users.noreply.github.com>
2018-07-08 06:19:50 +1000

    Language changes for #165. (#167)
    Made some changes to the language files, some of which are to fix grammatical errors, some of which are to add (what I hope are helpful) explanations, some of which are to remove redundant or unnecessary wording, and some of which are simply (in my opinion) improved phrasings.
    
    Probably the most significant part of these changes is that I have introduced the term "Trophy post" to describe the post with most likes/thanks for each user. If you guys don't like that term, we can change it.
    
    Also made changes to README.md, the most significant of which is to expand on the upgrade procedure, explaining that when new settings are involved, the plugin must first be uninstalled prior to upgrade, not just deactivated.

--- README.md
+++ README.md
@@ -1,29 +1,48 @@
-<strong>Thank you/like plugin</strong>
-====================
-- This is one of the best plugins for MyBB. It needs some love, so we (<strong>Eldenroot</strong>, <strong>SvePu</strong>, <strong>Whiteneo</strong> and <strong>Laird</strong>) decided to fix known bugs and make it even better. We have added many useful features including MyAlerts support. Give it a try and let us know what you think!
+The Thank You/Like System plugin for MyBB 1.8.x
+===============================================
 
+This is one of the best plugins for MyBB. It needed some love, so its current maintainers decided to adopt it to fix known bugs and make it even better. We have added many useful features, including MyAlerts support. Give it a try and let us know what you think!
 
-- </strong>MyBB MOD SITE link: http://community.mybb.com/mods.php?action=view&pid=360 </strong>
-- MyBB forum topic: http://community.mybb.com/thread-169382.html
-- This plugin was originally created by <strong>-G33K-</strong> and modified for MyBB 1.8.x by <strong>ATofighi</strong> (JavaScript part). Now it is maintained by <strong>Eldenroot</strong>, <strong>SvePu</strong>, <strong>Whiteneo</strong> and <strong>Laird</strong>!
+For more information, see the plugin's:
 
+- [Extend MyBB page](http://community.mybb.com/mods.php?action=view&pid=360).
+- [MyBB Community Forums topic](http://community.mybb.com/thread-169382.html).
 
-- All credits go to the original author <strong>-G33K-</strong>. Without you it could not be possible :)
+Credits
+-------
 
-<strong>FEEL FREE TO CONTRIBUTE/REPORT A BUG OR TELL US YOUR IDEA!</strong>
+This plugin was originally created by <strong>-G33K-</strong> and modified for MyBB 1.8.x by <strong>ATofighi</strong> (the JavaScript part). It is currently maintained by <strong>Eldenroot</strong>, <strong>SvePu</strong>, <strong>Whiteneo</strong>, and <strong>Laird</strong>!
 
-Thank you/Like plugin - MyBB 1.8.x 
+Full credit goes to the original author, <strong>-G33K-</strong>. Without you, this plugin would not have been possible. :)
 
------------------------------------------------------------------
+Contributing
+------------
 
-<strong>FAQ:</strong>
-- How to completely uninstall Thank You/Like System, including data from DB?
+Feel free to contribute, report a bug, or tell us your ideas!
 
-****** Click to deactivate and uninstall button in your ACP plugin tab -> choose "YES" to drop all tables from database. This cannot be reverted. All data will be lost.
-- How to update plugin to a newer version without loosing data?
+FAQ
+---
 
-****** Deactivate -> Copy new files and overwrite the old ones -> Activate again. All data (received and given thanks/likes) will be saved and restored.
+<strong>How do I completely uninstall the Thank You/Like System plugin and remove all of its data from the database?</strong>
 
-- How to change icons displayed in buttons (postbit and postbit_classic)?
+Click the "Uninstall" button beside the Thank You/Like System plugin in the "Plugins" module of the ACP and choose "YES" to drop all plugin data from database. This cannot be reverted. <strong>All</strong> data will be lost, including received and given thanks/likes, per-user counts of thanks/likes, overall thanks/likes statistics, plugin settings including per-usergroup settings, and any changes made to the plugin's CSS file.
 
-****** Replace both original icons with yours - images/thankyoulike/thx_add.png and thx_del.png
+<strong>How do I update the plugin to a newer version without losing data?</strong>
+
+If the updated version of the plugin does not contain new settings, then:
+
+1. Click the "Deactivate" button beside the Thank You/Like System plugin in the ACP "Plugins" module.
+2. Copy the new version's files, overwriting the old ones.
+3. Activate the plugin again via the ACP. All data will be retained: this includes received and given thanks/likes, per-user counts of thanks/likes, overall thanks/likes statistics, the plugin's settings including per-usergroup settings, and any changes made to the plugin's CSS file.
+
+If the updated version of the plugin <strong>does</strong> contain new settings, then:
+
+1. Make a note of your ACP settings for this plugin including per-usergroup settings (applicable when the plugin's "Usergroup-based thanks/likes limits" setting is enabled) and any changes you have made to the plugin's CSS file ("thankyoulike.css", accessible via the ACP under Templates & Style -> Themes).
+2. Click the "Uninstall" button beside the Thank You/Like System plugin in the ACP "Plugins" module and choose "NO" when asked whether to drop plugin data from the database.
+3. Copy the new version's files, overwriting the old ones.
+4. Install and activate the plugin again via the ACP.
+5. Reenter your settings for the plugin including any per-usergroup settings and any changes you had made to the plugin's CSS file (this is why it is important to have made a note of them before uninstalling the plugin).
+
+<strong>How do I change the icons of the buttons to add and remove thanks/likes?</strong>
+
+Replace the files thx_add.png and thx_del.png in the images/thankyoulike directory.
--- inc/languages/english/admin/config_thankyoulike.lang.php
+++ inc/languages/english/admin/config_thankyoulike.lang.php
@@ -5,130 +5,131 @@
  */
 
 $l['tyl_info_title'] = "Thank You/Like System";
-$l['tyl_info_desc'] = "Adds option for users to thank the user for the post or like the post.";
+$l['tyl_info_desc'] = "Adds an option for users to thank users for posts or to like posts.";
 $l['tyl_info_desc_url'] ="<br />*Edited and maintained for MyBB 1.8.x by: {1}, {2}, {3}, {4} and {5}<br />*Sources: {6}";
 $l['tyl_info_desc_recount'] = "Recount thanks/likes";
 $l['tyl_info_desc_configsettings'] = "Configure Settings";
-$l['tyl_info_desc_alerts_error'] = "<b>Thank You/Like System is uninstalled or deactivated</b>";
-$l['tyl_info_desc_alerts_integrate'] = "<b>Click <u>HERE</u> to integrate Thank You/Like System with MyAlerts</b>";
-$l['tyl_info_desc_alerts_integrated'] = "<b>Thank You/Like System and MyAlerts were integrated successfully!</b>";
+$l['tyl_info_desc_alerts_error'] = "<b>The Thank You/Like System is uninstalled or deactivated</b>";
+$l['tyl_info_desc_alerts_integrate'] = "<b>Click <u>HERE</u> to integrate the Thank You/Like System with MyAlerts</b>";
+$l['tyl_info_desc_alerts_integrated'] = "<b>The Thank You/Like System and MyAlerts were integrated successfully!</b>";
 $l['tyl_info_desc_alerts_registeralerttype'] = "Register a new alert type into MyAlerts";
 
 $l['tyl_title'] = "Thank You/Like System";
-$l['tyl_desc'] = "Settings to customize Thank You/Like System plugin.";
+$l['tyl_desc'] = "Settings to customize the Thank You/Like System plugin.";
 
 $l['tyl_enabled_title'] = "Enable/Disable";
 $l['tyl_enabled_desc'] = "Enable/Disable the Thank You/Like System.";
 
 $l['tyl_thankslike_title'] = "Thank you or like";
-$l['tyl_thankslike_desc'] = "Choose if you want to use thank you or like system.";
+$l['tyl_thankslike_desc'] = "Choose whether you want to use the thank you system or the like system (affects only wordings; does not affect functionality).";
 $l['tyl_thankslike_op_1'] = "Use thank you system";
 $l['tyl_thankslike_op_2'] = "Use like system";
 
 $l['tyl_firstall_title'] = "First post only or all posts";
-$l['tyl_firstall_desc'] = "Do you want the thanks/likes to be given on the first post of a thread only or on all the posts of a thread?";
+$l['tyl_firstall_desc'] = "Do you want to allow thanks/likes to be given to the first post of a thread only or to all posts?";
 $l['tyl_firstall_op_1'] = "First post only";
 $l['tyl_firstall_op_2'] = "All posts";
 
-$l['tyl_firstalloverwrite_title'] = "Special option for display thank/like buttons in ALL posts";
+$l['tyl_firstalloverwrite_title'] = "Special option to display thank/like buttons in ALL posts";
 $l['tyl_firstalloverwrite_desc'] = "Overwrite the above selected option All in certain forums (choose only forums, no categories!).";
 
 $l['tyl_removing_title'] = "Allow removing";
-$l['tyl_removing_desc'] = "Do you want to allow the removing of thank/like from a post already thanked/liked?";
+$l['tyl_removing_desc'] = "Do you want to allow the removing of a thank/like from a post already thanked/liked?";
 
-$l['tyl_tylownposts_title'] = "Allow giving thanks/likes to own posts";
-$l['tyl_tylownposts_desc'] = "Do you want to allow users to give thanks/likes to own posts?";
+$l['tyl_tylownposts_title'] = "Allow self thanks/likes";
+$l['tyl_tylownposts_desc'] = "Do you want to allow users to give thanks/likes to their own posts?";
 
-$l['tyl_remowntylfroms_title'] = "Remove own given thanks/likes from search results";
-$l['tyl_remowntylfroms_desc'] = "Choose YES to hide own given Thanks/Likes on search list.";
+$l['tyl_remowntylfroms_title'] = "Hide self-given thanks/likes from search results";
+$l['tyl_remowntylfroms_desc'] = "Choose \"Yes\" to hide self-given thanks/likes from user-level thanks/likes search listings.";
 
-$l['tyl_remowntylfromc_title'] = "Remove numbers of own given thanks/likes in counters";
-$l['tyl_remowntylfromc_desc'] = "Choose YES to hide own given thanks/likes in post and member profile counters.";
+$l['tyl_remowntylfromc_title'] = "Remove self-given thanks/likes from counters";
+$l['tyl_remowntylfromc_desc'] = "Choose \"Yes\" to hide self-given thanks/likes from thanks/likes counters in posts and member profiles.";
 
-$l['tyl_reputation_add_title'] = "Reputation points for given thanks/likes";
-$l['tyl_reputation_add_desc'] = "Do you want to add reputation points for given thanks/likes?";
+$l['tyl_reputation_add_title'] = "Add reputation points for thanks/likes";
+$l['tyl_reputation_add_desc'] = "Do you want to add reputation points when thanks/likes are given?";
 
 $l['tyl_reputation_add_reppoints_title'] = "Number of reputation points to add";
 $l['tyl_reputation_add_reppoints_desc'] = "Enter the number of reputation points you want to add for a thank/like (default value: 1).";
 
-$l['tyl_reputation_add_repcomment_title'] = "Comment of given reputation";
-$l['tyl_reputation_add_repcomment_desc'] = "Enter a text which will be displayed on thanked/liked users reputation overview (default value: empty).";
+$l['tyl_reputation_add_repcomment_title'] = "Reputation message";
+$l['tyl_reputation_add_repcomment_desc'] = "Enter the message to be displayed on the thanked/liked user's reputation overview (default value: empty).";
 
 $l['tyl_closedthreads_title'] = "Allow in closed threads";
-$l['tyl_closedthreads_desc'] = "Do you want to allow to give thanks/likes in closed threads?";
+$l['tyl_closedthreads_desc'] = "Do you want to allow thanks/likes to be given in closed threads?";
 
 $l['tyl_exclude_title'] = "Excluded forums";
-$l['tyl_exclude_desc'] = "Select forums where you do not want the threads and posts to use the thanks/likes system (only forums - no categories!).<br />Note: Recount of thanks/likes is not automatic, you have to do it manually through <a href=\"index.php?module=tools-recount_rebuild\">Recount & Rebuild in ACP</a> after you change and save this setting.";
+$l['tyl_exclude_desc'] = "Select forums in which giving thanks/likes is not permitted (select only forums - selecting categories will have no effect!).<br />Note: Thanks/likes are not recounted automatically when this setting is changed: you can recount them manually through <a href=\"index.php?module=tools-recount_rebuild\">Recount & Rebuild in the ACP</a> after you change and save this setting.";
 
 $l['tyl_exclude_count_title'] = "Forums excluded from counts";
-$l['tyl_exclude_count_desc'] = "Select forums in which you do not want any thanks/likes to count towards any member's publicly visible count of given and received thanks/likes (only forums - no categories!).<br />Note: Recount of thanks/likes is not automatic, you have to do it manually through <a href=\"index.php?module=tools-recount_rebuild\">Recount & Rebuild in ACP</a> after you change and save this setting.";
+$l['tyl_exclude_count_desc'] = "Select forums in which you do not want any thanks/likes to count towards any member's publicly visible count of given and received thanks/likes (select only forums - selecting categories will have no effect!).<br />Note: Thanks/likes are not recounted automatically when this setting is changed: you can recount them manually through <a href=\"index.php?module=tools-recount_rebuild\">Recount & Rebuild in the ACP</a> after you change and save this setting.";
 
 $l['tyl_unameformat_title'] = "Format usernames";
-$l['tyl_unameformat_desc'] = "Do you want to format the author's username and usernames in the thank/like list under the post according to their usergroups?";
+$l['tyl_unameformat_desc'] = "Do you want to format according to their usergroups the usernames of the post's author and its likers in the list of thanks/likes displayed under each post?";
 
-$l['tyl_hideforgroups_title'] = "Hide thank/like button";
-$l['tyl_hideforgroups_desc'] = "Select usergroups which cannot see thank/like button.";
+$l['tyl_hideforgroups_title'] = "Hide thank/like buttons per usergroup";
+$l['tyl_hideforgroups_desc'] = "Select usergroups to whose members the thank/like buttons should not be shown.";
 
 $l['tyl_showdt_title'] = "Show date/time";
-$l['tyl_showdt_desc'] = "Do you want to show date/time for thanks/likes in the list?";
-$l['tyl_showdt_op_1'] = "Not display";
+$l['tyl_showdt_desc'] = "How should the dates/times of thanks/likes in the list below each post be displayed?";
+$l['tyl_showdt_op_1'] = "Do not display";
 $l['tyl_showdt_op_2'] = "Display next to user name";
 $l['tyl_showdt_op_3'] = "Display on mouse hover over username";
 
 $l['tyl_dtformat_title'] = "Date/time format";
-$l['tyl_dtformat_desc'] = "Set the format you want to use to show the date/time in the thanks/likes list.<br />Format is same as the one used by PHP\'s date() function.<br />Example format: m-d-Y h:i A &lt;&lt;will show&gt;&gt; 12-31-2009 12:01 PM";
+$l['tyl_dtformat_desc'] = "Set the format you want to use to show the date/time in the thanks/likes list.<br />The format is as for PHP\'s <a href=\"php.net/manual/en/function.date.php\">date</a>() function.<br />Example format: <em>m-d-Y h:i A</em> &lt;&lt;will show&gt;&gt; <em>12-31-2009 12:01 PM</em>";
 
 $l['tyl_sortorder_title'] = "Sort order";
-$l['tyl_sortorder_desc'] = "Select the sort order for the thanks/likes list.";
+$l['tyl_sortorder_desc'] = "Select the sort order for the thanks/likes lists.";
 $l['tyl_sortorder_op_1'] = "Username ascending";
 $l['tyl_sortorder_op_2'] = "Username descending";
 $l['tyl_sortorder_op_3'] = "Date/time added ascending";
 $l['tyl_sortorder_op_4'] = "Date/time added descending";
 
-$l['tyl_collapsible_title'] = "Thank/like list collapsible";
-$l['tyl_collapsible_desc'] = "Do you want the thank/like list to be collapsible (show/hide ability)?";
+$l['tyl_collapsible_title'] = "Thanks/likes list collapsibility";
+$l['tyl_collapsible_desc'] = "Do you want the thanks/likes lists to be collapsible (with the ability to show/hide them)?";
 
 $l['tyl_colldefault_title'] = "Default collapsible state";
-$l['tyl_colldefault_desc'] = "If you want the list to be collapsible, what is the default state you want it in when the page loads, open or closed (hidden)?";
-$l['tyl_colldefault_op_1'] = "List shown";
-$l['tyl_colldefault_op_2'] = "List hidden (collapsed)";
+$l['tyl_colldefault_desc'] = "If you want the lists to be collapsible, in which default state do you want them to be when the page loads?";
+$l['tyl_colldefault_op_1'] = "Open (shown)";
+$l['tyl_colldefault_op_2'] = "Closed (hidden/collapsed)";
 
-$l['tyl_hidelistforgroups_title'] = "Hide thanks/likes list";
-$l['tyl_hidelistforgroups_desc'] = "Select usergroups which cannot see thanks/likes list.";
+$l['tyl_hidelistforgroups_title'] = "Hide thanks/likes lists per usergroup";
+$l['tyl_hidelistforgroups_desc'] = "Select usergroups to whose members the thanks/likes lists should not be shown.";
 
-$l['tyl_displaygrowl_title'] = "Display add/remove notification popup";
-$l['tyl_displaygrowl_desc'] = "Choose ON to enable the Ajax notification popup window.";
+$l['tyl_displaygrowl_title'] = "Display of add/remove notification popup";
+$l['tyl_displaygrowl_desc'] = "Choose \"On\" to enable popup notifications when a user clicks on a button to add/remove a thank/like. These pop up in the top right corner of the page and disappear automatically after a delay (they can also be closed manually). They indicate success or failure along with any error messages and any remaining limits.";
 
-$l['tyl_limits_title'] = "Enable usergroup based thanks/likes limits?";
-$l['tyl_limits_desc'] = "Choose YES to enable this feature and check the usergroup settings.";
-$l['tyl_flood_interval_title'] = "Flood prevention interval";
-$l['tyl_flood_interval_desc'] = "Enter the minimum number of seconds a member must wait between adding/removing thanks/likes. Set to zero to disable the requirement to wait at all.";
-$l['tyl_highlight_popular_posts_title'] = "Highlight posts with X numbers of thanks/likes?";
-$l['tyl_highlight_popular_posts_desc'] = "Choose YES to enable this feature and define your style of CSS class <strong>.popular_post</strong> in thankyoulike.css (in your theme).";
-$l['tyl_highlight_popular_posts_count_title'] = "Numbers of thanks/likes to highlight posts";
-$l['tyl_highlight_popular_posts_count_desc'] = "Set the number of thanks/likes the post will be highlighted.";
-
+$l['tyl_limits_title'] = "Enable usergroup-based thanks/likes limits?";
+$l['tyl_limits_desc'] = "Choose \"Yes\" to enable this feature. Settings for each usergroup can then be customised in the ACP under Users & Groups -> <a href=\"index.php?module=user-groups\">Groups</a> -> [Selected group] -> Users and Permissions [tab] -> Thanks/likes limits [heading]. The available settings are \"Flood prevention interval\" and \"Maximum thanks/likes allowed per 24 hours\".";
 $l['tyl_limits_permissions_system'] = "Thanks/likes limits";
 $l['tyl_limits_permissions_title'] = "Maximum thanks/likes allowed per 24 hours:";
-$l['tyl_limits_permissions_desc'] = "Here you can enter the maximum number of thanks/likes that users in this group can give per day (24 hours). To allow unlimited reputations per hour, enter 0.";
+$l['tyl_limits_permissions_desc'] = "The maximum number of thanks/likes that users in this group may give per day (24 hours). To allow unlimited thanks/likes per hour, enter 0.";
+$l['tyl_flood_interval_title'] = "Flood prevention interval";
+$l['tyl_flood_interval_desc'] = "The minimum number of seconds a member must wait between adding/removing thanks/likes. To disable the requirement to wait at all, enter zero.";
 
-$l['tyl_show_memberprofile_box_title'] = "Enable member profile box?";
-$l['tyl_show_memberprofile_box_desc'] = "Shows a box in a member profile page with user most thanked/liked post - Choose YES to enable it.";
-$l['tyl_profile_box_post_cutoff_title'] = "Cut off post message";
-$l['tyl_profile_box_post_cutoff_desc'] = "Set how many vars (symbols) you want to display in a post message (set 0 for no limit)";
-$l['tyl_profile_box_post_allowhtml_title'] = "Allow HTML";
-$l['tyl_profile_box_post_allowhtml_desc'] = "Selecting YES will allow HTML to be used in a post message.";
+$l['tyl_highlight_popular_posts_title'] = "Highlight posts with a given number of thanks/likes?";
+$l['tyl_highlight_popular_posts_desc'] = "Choose \"Yes\" to enable this feature. You can then customize the highlighted posts' style via the CSS class selector <strong>.popular_post</strong> in thankyoulike.css in your <a href\"index.php?module=style-themes\">theme</a>.";
+$l['tyl_highlight_popular_posts_count_title'] = "Thanks/likes count for highlight posts";
+$l['tyl_highlight_popular_posts_count_desc'] = "Posts with at least this many thanks/likes will be highlighted.";
+
+
+$l['tyl_show_memberprofile_box_title'] = "Enable trophy posts in member profiles?";
+$l['tyl_show_memberprofile_box_desc'] = "When enabled (choose \"Yes\"), each member's trophy post (most thanked/liked post) will be shown on his/her profile page.";
+$l['tyl_profile_box_post_cutoff_title'] = "Trophy post character cut-off";
+$l['tyl_profile_box_post_cutoff_desc'] = "Set the maximum number of (Unicode) characters to display in profile page trophy posts (0 for no limit).";
+$l['tyl_profile_box_post_allowhtml_title'] = "Allow HTML in trophy posts?";
+$l['tyl_profile_box_post_allowhtml_desc'] = "Selecting \"Yes\" will enable the rendering of HTML in profile page trophy posts.";
 $l['tyl_profile_box_post_allowmycode_title'] = "Allow MyCode";
-$l['tyl_profile_box_post_allowmycode_desc'] = "Selecting YES will allow MyCode to be used in a post message.";
+$l['tyl_profile_box_post_allowmycode_desc'] = "Selecting \"Yes\" will enable the rendering of MyCodes in profile page trophy posts.";
 $l['tyl_profile_box_post_allowsmilies_title'] = "Allow Smilies";
-$l['tyl_profile_box_post_allowsmilies_desc'] = "Selecting YES will allow Smilies to be used in a post message.";
+$l['tyl_profile_box_post_allowsmilies_desc'] = "Selecting \"Yes\" will enable the rendering of Smilies in profile page trophy posts.";
 $l['tyl_profile_box_post_allowimgcode_title'] = "Allow [img] Code";
-$l['tyl_profile_box_post_allowimgcode_desc'] = "Selecting YES will allow [img] Code to be used in a post message.";
+$l['tyl_profile_box_post_allowimgcode_desc'] = "Selecting \"Yes\" will enable the rendering of [img] MyCodes in profile page trophy posts.";
 $l['tyl_profile_box_post_allowvideocode_title'] = "Allow [video] Code";
-$l['tyl_profile_box_post_allowvideocode_desc'] = "Selecting YES will allow [video] Code to be used in a post message.";
+$l['tyl_profile_box_post_allowvideocode_desc'] = "Selecting \"Yes\" will enable the rendering of [video] MyCodes in profile page trophy posts.";
 
 $l['tyl_uninstall'] = 'Thank You/Like System - uninstallation';
-$l['tyl_uninstall_message'] = 'Do you wish to drop ALL plugin data entries from the database?';
+$l['tyl_uninstall_message'] = 'Do you wish to drop ALL plugin data from the database? (Selecting "No" will leave untouched thanks/likes given and received, per-user counts of thanks/likes, and overall thanks/likes statistics. It will not, however, prevent the removal of plugin settings (including any per-usergroup settings), nor will it prevent the removal of the plugin\'s CSS file, "thankyoulike.css" (including any changes you\'ve made to it), accessible via the ACP under Templates & Style -> <a href="index.php?module=style-themes">Themes</a>).';
 
 $l['setting_thankyoulike_promotion_rcv'] = 'Thanks/likes received';
 $l['setting_thankyoulike_promotion_rcv_desc'] = 'Enter the number of received thanks/likes required. Thanks/Likes count must be selected as a required value for this to be included. Select the type of comparison for thanks/likes.';
